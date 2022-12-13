54addaa0e5cf6183b8acdfea67fad34e175126b1
EEKONG <100251825+EEKONG@users.noreply.github.com>
2022-12-12 23:09:18 -0500

    Add files via upload

---
+++ IGproject1-server/config.js
@@ -0,0 +1,5 @@
+module.exports = {
+  MONGODB:
+    'mongodb+srv://EdiIGproject:IGproject@cluster0.8xfsifx.mongodb.net/IGproject?retryWrites=true&w=majority',
+  SECRET_KEY: 'some very secret key'
+};
